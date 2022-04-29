6c289512a8b5d87092b37ea064e21c58fa8569af
Yasin <yasinadan1@gmail.com>
2022-04-28 20:01:51 -0500

    react-router-dom implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/home';
+import Detail from './components/restrauntDetail';
+import Update from './components/updateRestraunt';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+        <Route path="/restraunts/:id" element={<Detail />} />
+        <Route path="/restraunts/:id/update" element={<Update />}/>
+        <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
